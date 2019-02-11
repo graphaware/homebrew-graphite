67af3e888d7090fac36e12587d6d1e0947b2993f
Vince Bickers <vince@graphaware.com>
2019-02-10 20:27:40 -0600

    ...

--- graphite.rb
+++ graphite.rb
@@ -2,13 +2,12 @@ class Graphite < Formula
   desc 'Rapid prototype of Spring Boot Neo4j apps from a graph schema'
   homepage 'http://graphaware.com'
   url "https://www.dropbox.com/s/15dv9ln06ihlkpe/graphite.tgz?dl=1"
-  version '0.9'
+  version '0.9.1'
 
   depends_on :java => "1.8"
 
   def install
     libexec.install Dir['*']
-    bin.install_symlink "#{libexec}/lib" => "lib"
   end
 
 end
