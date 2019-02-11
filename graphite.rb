4587574838fe7cbbd85fff49362da20045e36817
Vince Bickers <vince@graphaware.com>
2019-02-10 18:45:14 -0600

    Set version

--- graphite.rb
+++ graphite.rb
@@ -2,7 +2,7 @@ class Graphite < Formula
   desc 'Rapid prototype of Spring Boot Neo4j apps from a graph schema'
   homepage 'http://graphaware.com'
   url "https://www.dropbox.com/s/15dv9ln06ihlkpe/graphite.tgz"
-  version 0.1
+  version '0.1'
 
   depends_on :java => "1.8"
 
