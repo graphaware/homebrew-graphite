2bd42c265853b83ac1f011ef58f2fa31a66f94f2
Vince Bickers <vince@graphaware.com>
2019-02-10 22:14:26 -0600

    Yet another way...

--- graphite.rb
+++ graphite.rb
@@ -2,14 +2,22 @@ class Graphite < Formula
   desc 'Rapid prototype of Spring Boot Neo4j apps from a graph schema'
   homepage 'http://graphaware.com'
   url "https://www.dropbox.com/s/15dv9ln06ihlkpe/graphite.tgz?dl=1"
-  version '0.9.4'
+  version '0.9.5'
 
   depends_on :java => "1.8"
 
   def install
-    ENV["GRAPHITE_HOME"] = libexec
-    libexec.install Dir['*']
-    bin.env_script_all_files(libexec/"bin", :GRAPHITE_HOME => ENV["GRAPHITE_HOME"])
+    chmod 0755, "bin/graphite"
+
+    inreplace ["bin/graphite"] do |s|
+      s.gsub! /^GRAPHITE_HOME=.*$/, "GRAPHITE_HOME=#{libexec}"
+    end
+
+    bin.install "bin/graphite"
+    libexec.install Dir["*"]
+    # Add symlinks for the Language Server
+    prefix.install_symlink libexec/"lib"
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
   end
 
 end
