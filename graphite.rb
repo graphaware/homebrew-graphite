435d847fabde25312733024f8c406e28facc5333
Vince Bickers <vince@graphaware.com>
2019-02-10 21:36:41 -0600

    And now for something completely different

--- graphite.rb
+++ graphite.rb
@@ -2,13 +2,15 @@ class Graphite < Formula
   desc 'Rapid prototype of Spring Boot Neo4j apps from a graph schema'
   homepage 'http://graphaware.com'
   url "https://www.dropbox.com/s/15dv9ln06ihlkpe/graphite.tgz?dl=1"
-  version '0.9.2'
+  version '0.9.3'
 
   depends_on :java => "1.8"
 
   def install
+    ENV["GRAPHITE_HOME"] = libexec
     libexec.install Dir['*']
-    bin.install_symlink "#{libexec}/bin/graphite" => "graphite"
+    bin.install Dir['#{libexec}/bin/graphite']
+    bin.env_script_all_files(libexec/"bin", :GRAPHITE_HOME => ENV["GRAPHITE_HOME"])
   end
 
 end
