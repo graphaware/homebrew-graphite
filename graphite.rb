22aba5f958dcb76ba6e6be344afd2e2ed82725c5
Vince Bickers <vince@graphaware.com>
2019-02-10 20:34:14 -0600

    Install is correct, symlink needs to be setup

--- graphite.rb
+++ graphite.rb
@@ -2,12 +2,13 @@ class Graphite < Formula
   desc 'Rapid prototype of Spring Boot Neo4j apps from a graph schema'
   homepage 'http://graphaware.com'
   url "https://www.dropbox.com/s/15dv9ln06ihlkpe/graphite.tgz?dl=1"
-  version '0.9.1'
+  version '0.9.2'
 
   depends_on :java => "1.8"
 
   def install
     libexec.install Dir['*']
+    bin.install_symlink "#{libexec}/bin/graphite" => "graphite"
   end
 
 end
