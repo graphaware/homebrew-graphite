7e069d93e51a16e4a05c1afb08cecbc3571e8fe7
Vince Bickers <vince@graphaware.com>
2019-02-10 18:42:28 -0600

    First commit

---
+++ graphite.rb
@@ -0,0 +1,15 @@
+class Graphite < Formula
+  desc 'Rapid prototype of Spring Boot Neo4j apps from a graph schema'
+  homepage 'http://graphaware.com'
+  url "https://www.dropbox.com/s/15dv9ln06ihlkpe/graphite.tgz"
+  version 0.1
+
+  depends_on :java => "1.8"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install Dir['*']
+    bin.install Dir["#{libexec}/bin/graphite"]
+  end
+
+end
