0c3a8f0ef831b3d46ecb4b82b952f4a9c2db71e7
Vince Bickers <vince@graphaware.com>
2019-02-10 19:37:15 -0600

    More syntax fixes

--- graphite.rb
+++ graphite.rb
@@ -2,12 +2,12 @@ class Graphite < Formula
   desc 'Rapid prototype of Spring Boot Neo4j apps from a graph schema'
   homepage 'http://graphaware.com'
   url "https://www.dropbox.com/s/15dv9ln06ihlkpe/graphite.tgz?dl=1"
-  version '0.4'
+  version '0.5'
 
   depends_on :java => "1.8"
 
   def install
-    bin.install bin/graphite
+    bin.install "bin/graphite"
   end
 
 end
