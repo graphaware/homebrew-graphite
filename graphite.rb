93a45748a2e151b9caef42012707a273379a39cb
Vince Bickers <vince@graphaware.com>
2019-02-10 19:10:39 -0600

    Add symlink

--- graphite.rb
+++ graphite.rb
@@ -2,14 +2,14 @@ class Graphite < Formula
   desc 'Rapid prototype of Spring Boot Neo4j apps from a graph schema'
   homepage 'http://graphaware.com'
   url "https://www.dropbox.com/s/15dv9ln06ihlkpe/graphite.tgz?dl=1"
-  version '0.1'
+  version '0.2'
 
   depends_on :java => "1.8"
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install Dir['*']
-    bin.install Dir["#{libexec}/bin/graphite"]
+    bin.install_symlink "#{libexec}/{#version}/bin/graphite" => "graphite"
   end
 
 end
